85e9e1715518a893596df5c2c453c0161ded7c6b
yongjie <753516036@qq.com>
2017-03-20 08:39:59 +0800

    。。

---
+++ BaiduMapDemo/app/src/main/java/com/example/jie/baidumapdemo/MyApp.java
@@ -0,0 +1,27 @@
+package com.example.jie.baidumapdemo;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.baidu.location.LocationClient;
+import com.baidu.mapapi.SDKInitializer;
+
+/**
+ * Created by jie on 2017/3/19.
+ */
+
+public class MyApp extends Application {
+    public static Context mContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mContext=getApplicationContext();
+        SDKInitializer.initialize(mContext);
+    }
+
+
+    public static Context getmContext() {
+        return mContext;
+    }
+}
