233183af06e25d8ac37cb9a7012e325e15980085
于永杰 <jieleo@yuyongjiedeMacBook-Pro.local>
2017-03-27 11:28:44 +0800

    ..

---
+++ MediaPlayer/app/src/main/java/com/lanou/dllo/mediaplayer/SeondActivity.java
@@ -0,0 +1,13 @@
+package com.lanou.dllo.mediaplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SeondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seond);
+    }
+}
